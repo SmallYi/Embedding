1da96c1be749b3f91b9ec8fd6a8113fed808ffee
ycachy <1021487797@qq.com>
2018-12-31 03:22:35 +0200

    Add files via upload

---
+++ tsvd/startup.m
@@ -0,0 +1,13 @@
+run('C:\Users\ChenKx\Desktop\tensorsvd\pkg\vlfeat-0.9.20\toolbox\vl_setup.m');
+addpath('C:\Users\ChenKx\Desktop\tensorsvd\pkg\')
+addpath('C:\Users\ChenKx\Desktop\tensorsvd\pkg\tensor_toolbox_2.6');
+addpath('C:\Users\ChenKx\Desktop\tensorsvd\pkg\tensor_toolbox_2.6\met');
+addpath('C:\Users\ChenKx\Desktop\tensorsvd\pkg\PROPACK')
+
+
+%% LIBRARIES
+addpath('C:\Users\ChenKx\Desktop\tensorsvd\libs\poblano_toolbox_1.1\');
+addpath('C:\Users\ChenKx\Desktop\tensorsvd\libs\tensor_toolbox_2.5\');
+addpath('C:\Users\ChenKx\Desktop\tensorsvd\libs\nway331\');
+%addpath(genpath('src'));
+addpath(genpath('function'))
