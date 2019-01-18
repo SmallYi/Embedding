3296102f17d9337505faff4737151a5ab9ba2579
ycachy <1021487797@qq.com>
2019-01-18 14:06:33 +0800

    Update nodemat.py

--- RawExtraction/nodemat.py
+++ RawExtraction/nodemat.py
@@ -63,10 +63,10 @@ for file in files:
 
 
         #j=j+1
-            scipy.io.savemat('att/'+str(file)+'.mat', mdict={'network': network})
-            import mat4py
-            sssssssssssssss=mat4py.loadmat('att/'+'nodeopenssl1.txt'+'.mat')
-            ssssss=sssssssssssssss['network']
+          #  scipy.io.savemat('att/'+str(file)+'.mat', mdict={'network': network})
+           # import mat4py
+           # sssssssssssssss=mat4py.loadmat('att/'+'nodeopenssl1.txt'+'.mat')
+          #  ssssss=sssssssssssssss['network']
             #print ssssss
 
 
