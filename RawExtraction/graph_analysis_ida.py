a92806ffbc261eff6d0447855b21a7ea8df77ce9
ycachy <1021487797@qq.com>
2019-02-01 12:12:12 +0800

    Add files via upload

--- RawExtraction/graph_analysis_ida.py
+++ RawExtraction/graph_analysis_ida.py
@@ -84,44 +84,43 @@ def getFuncCalls(func):
 def getcodesquence(bl):
 	sequence=[]
 	#print("EEEEEEEEEEEEEEEEEEEEEEEE")
-	x86_AI = {'add':1, 'sub':1, 'div':1, 'imul':1, 'idiv':1, 'mul':1, 'shl':1, 'dec':1, 'inc':1}
-	mips_AI = {'add':1, 'addu':1, 'addi':1, 'addiu':1, 'mult':1, 'multu':1, 'div':1, 'divu':1}
-	x86_TI = {'jmp': 1, 'jz': 1, 'jnz': 1, 'js': 1, 'je': 1, 'jne': 1, 'jg': 1, 'jle': 1, 'jge': 1, 'ja': 1, 'jnc': 1,
+	#x86_AI = {'add':1, 'sub':1, 'div':1, 'imul':1, 'idiv':1, 'mul':1, 'shl':1, 'dec':1, 'inc':1}
+	#mips_AI = {'add':1, 'addu':1, 'addi':1, 'addiu':1, 'mult':1, 'multu':1, 'div':1, 'divu':1}
+	#x86_TI = {'jmp': 1, 'jz': 1, 'jnz': 1, 'js': 1, 'je': 1, 'jne': 1, 'jg': 1, 'jle': 1, 'jge': 1, 'ja': 1, 'jnc': 1,
 			  'call': 1}
-	mips_TI = {'beq': 1, 'bne': 1, 'bgtz': 1, "bltz": 1, "bgez": 1, "blez": 1, 'j': 1, 'jal': 1, 'jr': 1, 'jalr': 1}
-	arm_TI = {'MVN': 1, "MOV": 1}
+	#mips_TI = {'beq': 1, 'bne': 1, 'bgtz': 1, "bltz": 1, "bgez": 1, "blez": 1, 'j': 1, 'jal': 1, 'jr': 1, 'jalr': 1}
+	#arm_TI = {'MVN': 1, "MOV": 1}
 	# in each opcode, there are three lines, one is the arm, one is the x86, and the other is mips.
-	GDTI={'MVN':2, "MOV":1,'LDR':3,'LDRB':4,'LDRH':5,'STR':6,'STRB':7,'STRH':8,'LDM':9,'STM':9,'SWP':10,'SWPB':11,
-		  'mov':1,'movsx':2,'movzx':3,'push':4,'pop':5,'push':6,'pusha':7,'pope':8,'pushed':9,'popad':10,'bswap':11,'xchg':12,'cmpxchg':13,'xadd':14,'xlat':15,'mvn':16,'cmp':17,'tst':18,'tea':19,
-		  'lui':1,'lw':2,'sw':3}
-
+	GDTI={'MVN':1, "MOV":1,'LDR':2,'LDRB':2,'LDRH':2,'STR':3,'STRB':3,'STRH':3,'LDM':2,'STM':3,'SWP':3,'SWPB':3,
+		  'mov':1,'movsx':1,'movzx':1,'push':4,'pop':5,'push':4,'pusha':4,'pope':5,'pushed':4,'popad':5,'bswap':3,'xlat':7,'mvn':1,'popf':5,'pushd':4,'popd':5，'movs':1,'lods':2,'stos':3,
+		  'lui':1,'lw':1,'sw':3}
 	IOPTI={'in':1,'out':2,}
-	DATI={'lea':1,'lds':2,'les':3,'lfs':4,'lgs':5,'les':6}
-	FTI={'lahf':1,'sahf':2,'push':3,'popf':4,'pushd':5,'popd':6}
-	ADO={'add':1,'adc':2,'inc':3,'aaa':4,'daa':5,'sub':6,'sbb':7,'dec':8,'nec':9,'cmp':10,
-		 'ADD':1, 'ADC':2,'SUB':3,'C':4,'R':5,'RSC':6,'TEQ':8, 'TST':9,'CMP':10,'MUL':13,'MLA':12,'SMULL':11,'SMLAL':14,'UMULL':15,'UMLAL':16,
-		 'aas':11,'das':12,'mul':13,'imul':14,'aam':15,'div':16,'idiv':17,'aad':18,'cbw':19,
-		 'cwd':20,'cwde':21,'cdq':22,'c':23,'r':24,'rsc':25,'mla':13,'smull':14,'umull':15,'umlal':16,
-		 'add':1,'addu':2,'sub':6,'subu':7,'addi':1,'addiu':2}
-	LDO={'and':1,'or':2,'xor':3,'not':4,'test':5,'shl':6,'sal':7,'shr':8,'sar':9,'rol':10,'ror':11,'rcl':12,'rcr':13,
-		 'orr':14,'eor':15,'bic':16,
-		 'and':1,'or':2,'xor':3,'nor':4,'andi':5,'ori':6,'xori':7,
-		 'AND':1,'ORR':2,'EOR':3,'BIC':4,'LSL':5,'LSR':6,}
-	SI={'movs':1,'cmps':2,'scas':3,'lods':4,'stos':5,'rep':6,'repe':7,'repen':8,'repc':9,'repnc':10}
-	UBI={'jump':1,'call':2,'ret':3,
-		 'j':1,'jal':2,'jr':3}
-	CBI={'ja':1,'jnz':15,'jnbe':14,'jae':2,'jnb':10,'jb':3,'jbe':4,'jg':5,'jge':6,'jl':7,'jle':8,'je':9,'jne':11,'jc':12,'jnc':13,'jno':14,'jnp':15,'jns':16,'jo':17,'jp':18,'js':19,
-		 'slt':1,'sltu':2,'sll':3,'srl':4,'sra':5,'sllv':6,'srlv':7,'srav':8,"beq":9, "bne":10,'slti':11,'sltiu':12,
+	DATI={'lea':1,'lds':1,'les':1,'lfs':1,'lgs':1}
+	FTI={'lahf':1,'sahf':2,}
+	ADO={'add':1,'adc':1,'inc':2,'aaa':3,'daa':3,'sub':4,'sbb':4,'dec':5,'nec':5,'xadd':1,'tst':6,'tea':6,'cmp':7,'xchg':7,'cmps':7, 'cmpxchg':7,
+		 'ADD':1, 'ADC':1,'SUB':4,'C':4,'R':5,'RSC':5,'TEQ':6,'TST':6,'CMP':7,'MUL':8,'MLA':8,'SMULL':8,'SMLAL':8,'UMULL':8,'UMLAL':8,
+		 'aas':3,'das':3,'mul':8,'imul':8,'aam':3,'div':4,'idiv':4,'aad':1,'cbw':9,
+		 'cwd':9,'cwde':9,'cdq':9,'c':9,'r':10,'rsc':10,'mla':8,'smull':8,'umull':8,'umlal':8,
+		 'add':1,'addu':1,'sub':4,'subu':4,'addi':1,'addiu':1}
+	LDO={'and':1,'or':2,'xor':2,'not':3,'test':5,'shl':6,'sal':6,'shr':6,'sar':6,'rol':7,'ror':7,'rcl':7,'rcr':7,
+		 'orr':7,'eor':7,'bic':8,
+		 'and':1,'or':2,'xor':2,'nor':2,'andi':3,'ori':2,'xori':2,
+		 'AND':1,'ORR':2,'EOR':2,'BIC':8,'LSL':3,'LSR':3,}
+	SI={'scas':1,'rep':2,'repe':2,'repen':2,'repc':2,'repnc':2}
+	UBI={'jump':1,'call':2,'ret':2,
+		 'j':1,'jal':1,'jr':1}
+	CBI={'ja':1,'jnz':1,'jnbe':2,'jae':2,'jnb':2,'jb':3,'jbe':3,'jg':4,'jge':4,'jl':5,'jle':5,'je':1,'jne':2,'jc':6,'jnc':6,'jno':6,'jnp':6,'jns':6,'jo':7,'jp':7,'js':7,
+		 'slt':1,'sltu':1,'sll':1,'srl':2,'sra':2,'sllv':1,'srlv':2,'srav':2,"beq":3, "bne":3,'slti':1,'sltiu':1,
 		 #"beqz":1 "bgez":4, "b":5, "bnez":6, "bgtz":7, "bltz":8, "blez":9, "bgt":10, "bge":11, "blt":12, "ble":13, "bgtu":14, "bgeu":15, "bltu":16, "bleu":17,
-		 "B":1, "BAL":2, "BNE":3, "BEQ":4, "BPL":5, "BMI":6, "BCC":7, "BLO":8, "BCS":9, "BHS":10, "BVC":11, "BVS":12, "BGT":13, "BGE":14, "BLT":15, "BLE":16, "BHI":17 ,"BLS":18,'BL':19,'BLX':20}
-	LCI={'loop':1,'loope':2,'loopne':3,'jcxz':4,'jecxz':5}
-	II={'int':1,'into':2,'iret':3,
+		 "B":1, "BAL":1, "BNE":1, "BEQ":1, "BPL":2, "BMI":2, "BCC":3, "BLO":5, "BCS":3, "BHS":4, "BVC":6, "BVS":6, "BGT":7, "BGE":7, "BLT":5, "BLE":5, "BHI":4 ,"BLS":5,'BL':5,'BLX':5}
+	LCI={'loop':1,'loope':1,'loopne':1,'jcxz':2,'jecxz':2}
+	II={'int':1,'into':1,'iret':2,
 		'SWI':1,'BKPT':2,}
-	PCI={'hlt':1,'wait':2,'esc':3,'lock':4,'nop':5,'stc':6,'clc':7,'cmc':8,'std':9,'cld':10,'sti':11,'cli':12,
-		 'CDP':1,'LDC':2,'STC':3,'MCR':4,'MRC':5,'MRS':6,'MSR':7}
-	PI={'dw':1,'proc':2,'ends':3,'segment':4,'assume':5,'ends':6,'end':7,
-		'GBLA':1,'GBLL':2,'GBLS':3,'LCLA':4,'LCLL':5,'LCLS':6,'SETA':7,'SETL':8,'SETS':9,'RLIST':10,'DCB':11,'DCW':12,'DCD':13,'DCFD':14,
-		'DCFS':15,'DCQ':16,'SPACE':17,'MAP':18,'FILED':19}
+	PCI={'hlt':1,'wait':2,'esc':3,'lock':4,'nop':5,'stc':6,'clc':7,'cmc':7,'std':8,'cld':7,'sti':8,'cli':7,
+		 'CDP':1,'LDC':2,'STC':3,'MCR':4,'MRC':4,'MRS':4,'MSR':4}
+	PI={'dw':1,'proc':2,'ends':3,'segment':4,'assume':5,'end':3,
+		'GBLA':1,'GBLL':1,'GBLS':1,'LCLA':2,'LCLL':2,'LCLS':2,'SETA':3,'SETL':3,'SETS':3,'RLIST':4,'DCB':5,'DCW':5,'DCD':5,'DCFD':5,
+		'DCFS':5,'DCQ':5,'SPACE':6,'MAP':7,'FILED':8}
 	opcodenum=0
 	#blocks=[(v.startEA, v.endEA) for v in FlowChart(func)]
 
